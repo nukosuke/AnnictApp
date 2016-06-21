1993ab29b1e463124da9e159a1007de4447bde5b
nukosuke <nukosuke@shibuya.tech>
2016-06-21 17:32:19 +0900

    add constants

---
+++ common/constants/client-constants.js
@@ -0,0 +1,6 @@
+/**
+ * client configuration
+ */
+export const AUTHORIZE_URI = 'https://api.annict.com/oauth/authorize';
+export const CLIENT_ID     = '8e5b68cd11f68d2734b350f31c3dc568f5aa81009b531f82a99bd6f5027d7a3d';
+export const REDIRECT_URI  = 'urn:ietf:wg:oauth:2.0:oob';
