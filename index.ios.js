8eac346c8e10a723a0dccc0392086a9d57c575e5
nukosuke <nukosuke@shibuya.tech>
2016-06-22 22:14:20 +0900

    display record timeline username

--- index.ios.js
+++ index.ios.js
@@ -1,31 +1,123 @@
 /**
- * Sample React Native App
+ * Annict App
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, { Component } from 'react';
+import Realm from 'realm';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS,
+  TabBarIOS
 } from 'react-native';
+import { LoginModal } from './common/login-modal';
+import { ListView } from 'realm/react-native';
+import {
+  User,
+  Token,
+  Work,
+  Episode,
+  Record,
+  RecordList,
+} from './common/schema';
+
+import Annict from 'annict';
+
+import {
+  AUTHORIZE_URI,
+  CLIENT_ID,
+  REDIRECT_URI
+} from './common/constants/client-constants'
 
 class AnnictApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.annict = new Annict();
+    this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    this.realm = new Realm({
+      path: 'annict.realm',
+      schema: [User, Token, Work, Episode, Record, RecordList]
+    });
+
+    this.state = {
+      access_token: null,
+      records: this.ds.cloneWithRows([]),
+    }
+  }
+
+  componentWillMount() {
+    const token = this.realm.objects('Token')[0];
+
+    if(token) {
+      this.state.access_token = token.access_token;
+      this.annict.client.setHeader('Authorization', `Bearer ${token.access_token}`);
+    }
+    else {
+      //TODO: set visible authenticate modal by props
+      console.log('no token');
+    }
+  }
+
+  componentDidMount() {
+    this.annict.Record.get({})
+    .then(body => {
+      //this.setState({ records: body.records });
+      console.log(body);
+      this.setState({ records: this.ds.cloneWithRows(body.records) });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .done();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <LoginModal realm={this.realm} />
+
+        <NavigatorIOS
+          barTintColor='#333'
+          titleTextColor='#eee'
+          initialRoute={{
+            component: View,
+            title: 'Annict',
+            //pass
+          }}
+        />
+
+        <View style={{flex: 1, paddingTop: 64}}>
+          <ListView
+            dataSource={this.state.records}
+            renderRow={(rowData) => <Text>{rowData.user.name}</Text>}
+          />
+        </View>
+
+        <TabBarIOS
+          unselectedTintColor='#666'
+          tintColor='#f51234'
+          barTintColor='white'>
+          <TabBarIOS.Item
+            title='放送予定'>
+          </TabBarIOS.Item>
+          <TabBarIOS.Item
+            title='記録'>
+          </TabBarIOS.Item>
+          <TabBarIOS.Item
+            title='作品'>
+          </TabBarIOS.Item>
+          <TabBarIOS.Item
+            title='records'>
+          </TabBarIOS.Item>
+          <TabBarIOS.Item
+            title='設定'>
+          </TabBarIOS.Item>
+        </TabBarIOS>
       </View>
     );
   }
@@ -34,17 +126,9 @@ class AnnictApp extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    backgroundColor: '#f9f9fb',
   },
   instructions: {
-    textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
