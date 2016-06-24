fd908c0911edef3c8f8f0f8a5836c515162c78ee
nukosuke <nukosuke@shibuya.tech>
2016-06-25 00:55:02 +0900

    add search form

--- common/works-pane.js
+++ common/works-pane.js
@@ -12,25 +12,27 @@ export class WorksPane extends Component {
 
   render() {
     return (
-      <View style={{marginTop: 65}}>
-        <View style={{padding: 4}}>
-        <TextInput
-          style={{height: 30, borderColor: '#e1e1e1', borderWidth: 1}}
-          placeholder='キーワードで検索...'
-        />
+      <View>
+        <View style={{paddingHorizontal: 8, paddingTop: 24, paddingBottom: 8, backgroundColor: '#444'}}>
+          <TextInput
+            style={{height: 25, borderRadius: 2, borderColor: '#e1e1e1', borderWidth: 1, backgroundColor: '#eee', padding: 4}}
+            placeholder='キーワードで検索...'
+          />
         </View>
-        <ListView
-          enableEmptySections={true}
-          dataSource={this.props.works}
-          renderRow={(rowData) => {
-            return (
-              <View style={theme.cardStyle}>
-                <Text>rowData.title</Text>
-              </View>
-            );
-          }}
-        />
-        <Text>aaaa</Text>
+
+        <View style={{marginTop: 65}}>
+          <ListView
+            enableEmptySections={true}
+            dataSource={this.props.works}
+            renderRow={(rowData) => {
+              return (
+                <View style={theme.cardStyle}>
+                  <Text>rowData.title</Text>
+                </View>
+              );
+            }}
+          />
+        </View>
       </View>
     );
   }
