3f4088bd2c3d79fc02095960682dfd0f4c738f6d
nukosuke <nukosuke@shibuya.tech>
2016-06-25 02:53:09 +0900

    add navigator

--- common/settings-pane.js
+++ common/settings-pane.js
@@ -1,21 +1,33 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Text } from 'react-native';
+import { NavigatorIOS, View, Text } from 'react-native';
 import { ListView } from 'realm/react-native';
 
 export class SettingsPane extends Component {
   render() {
     return (
-      <View style={{flex: 1, paddingTop: 65}}>
-        <ListView
-          dataSource={this.props.settings}
-          renderRow={rowData => {
-            return (
-              <View style={{paddingLeft: 16}}>
-                <Text>{rowData.name}</Text>
-              </View>
-            );
+      <View style={{flex: 1}}>
+        <NavigatorIOS
+          barTintColor='#444'
+          titleTextColor='#eee'
+          initialRoute={{
+            component: View,
+            title: '設定',
+            //pass
           }}
         />
+
+        <View style={{flex: 1, paddingTop: 65}}>
+          <ListView
+            dataSource={this.props.settings}
+            renderRow={rowData => {
+              return (
+                <View style={{paddingLeft: 16}}>
+                  <Text>{rowData.name}</Text>
+                </View>
+              );
+            }}
+          />
+        </View>
       </View>
     );
   }
