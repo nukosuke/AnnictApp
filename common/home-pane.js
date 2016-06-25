60c6ae1511df008fdd512d034f3496d7fc0b1122
nukosuke <nukosuke@shibuya.tech>
2016-06-25 20:14:50 +0900

    add title & episode number

--- common/home-pane.js
+++ common/home-pane.js
@@ -36,8 +36,11 @@ export class HomePane extends Component {
                 return (
                   <View style={{paddingTop: 4, paddingBottom: 4}}>
                     <View style={theme.cardStyle}>
-                      <Text style={{padding: 15}}>
-                        {rowData.user.name}
+                      <Text style={{paddingHorizontal: 15, paddingTop: 15}}>
+                        <Text style={{color: '#f85b73'}}>{rowData.user.name}</Text>
+                        <Text style={{color: '#666'}}> が </Text>
+                        <Text style={{color: '#f85b73'}}>{rowData.work.title} {rowData.episode.number_text}</Text>
+                        <Text style={{color: '#666'}}> を見ました</Text>
                       </Text>
                       <Text style={theme.cardContentStyle}>
                         {rowData.comment}
